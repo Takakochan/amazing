e8d69e65aae249a44811198cb8c5d0e1b57dc928
Ilya Claassen <ilclaass@f0r1s10.codam.nl>
2026-03-10 20:01:36 +0100

    feat(parse): parse file using multiple parsers

--- tk_function.py
+++ tk_function.py
@@ -2,8 +2,8 @@ from abc import ABC, abstractmethod
 
 
 class ConfigValue(ABC):
-    @abstractmethod
     @staticmethod
+    @abstractmethod
     def parse(value: str) -> int | str | bool | tuple[int, int]:
         pass
 
@@ -14,10 +14,15 @@ class IntValue(ConfigValue):
         return int(value)
 
 
-class StrValue(ConfigValue):
+class PositonValue(ConfigValue):
     @staticmethod
-    def parse(value: str) -> str:
-        return value
+    def parse(value: str) -> tuple[int, int]:
+        split_value = value.split(",", 1)
+        # TODO: check invalid input
+        x = IntValue.parse(split_value[0])
+        y = IntValue.parse(split_value[1])
+
+        return (x, y)
 
 
 class BoolValue(ConfigValue):
@@ -32,26 +37,46 @@ class BoolValue(ConfigValue):
         raise ValueError("Invalid input, must be True or False")
 
 
-class PositonValue(ConfigValue):
+class StrValue(ConfigValue):
     @staticmethod
-    def parse(value: str) -> tuple[int, int]:
-        split_value = value.split(",")
-        # TODO: check invalid input
-        x = IntValue.parse(split_value[0])
-        y = IntValue.parse(split_value[1])
-
-        return (x, y)
+    def parse(value: str) -> str:
+        return value
 
 
-def read_config():
-    with open("config.txt") as file:
-        for line in file:
-            stripped_line = line.strip()
+def read_config(filename: str) -> None:
+    config = {}
 
-            if stripped_line.startswith("#"):
-                continue
+    config["HEIGHT"] = IntValue
+    config["WIDTH"] = IntValue
+    config["ENTRY"] = PositonValue
+    config["EXIT"] = PositonValue
+    config["OUTPUT_FILE"] = StrValue
+    config["PERFECT"] = BoolValue
 
-            split_line = stripped_line.split("=")
-            # TODO: check invalid input
-            key = split_line[0]
-            value = split_line[1]
+    try:
+        with open(filename, encoding="utf-8") as file:
+            for line in file:
+                stripped_line = line.strip()
+
+                if not stripped_line or stripped_line.startswith("#"):
+                    continue
+
+                split_line = stripped_line.split("=", 1)
+                # TODO: check invalid input
+                key = split_line[0]
+                value = split_line[1]
+
+                parser = config.get(key)
+
+                if parser is None:
+                    raise ValueError(f"Invalid key: {key}")
+
+                result = parser.parse(value)
+
+                print(f"{key}={value} -> {result}")
+    except OSError as error:
+        print(f"{error}")
+
+
+if __name__ == "__main__":
+    read_config("config.txt")
