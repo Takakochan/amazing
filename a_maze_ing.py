e462ae20fbea2a53f214914127fb3a5d31779ddf
Ilya Claassen <ilclaass@f0r3s11.codam.nl>
2026-03-24 15:47:13 +0100

    chore: catch error in main

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,11 +2,16 @@
 
 import sys
 
+from config import ConfigError
 from maze import Maze
 
 
 def main() -> None:
-    maze = Maze.from_config_file(sys.argv[1])
+    try:
+        maze = Maze.from_config_file(sys.argv[1])
+    except ConfigError as error:
+        raise error
+
     maze.display()
     maze.save()
 
