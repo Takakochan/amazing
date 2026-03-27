82c46063e15d790aa9bd76b49a45f2dab2553a04
Takako Kunugi <tkunugi@f1r2s2.codam.nl>
2026-03-27 14:00:35 +0100

    astar started - create PriorutyQue class and

--- mazegen/solvers/a_star.py
+++ mazegen/solvers/a_star.py
@@ -1,6 +1,7 @@
 from mazegen.cell import Cell
 from mazegen.grid import Grid
 from mazegen.solvers.base import Solver
+import heapq
 
 
 class SolverAStar(Solver):
@@ -10,3 +11,26 @@ class SolverAStar(Solver):
         exit: Cell,  # noqa: A002
     ) -> None:
         pass
+
+class PriorityQue():
+    def __init__(self):
+        self._heap = []
+    
+    def push(self, priority: int, cell: Cell):
+
+    
+    # def pop(self) -> Cell:
+    #     return
+
+    # def is_empty(self)-> bool:
+    #     return 
+
+
+
+def f_scor(g_score: int, manhattan_heuristic: int) -> int:
+    return g_score + manhattan_heuristic
+        
+def manhattan_heuristic(current: Cell, exit: Cell) -> int:
+    return abs(current.x - exit.x) + abs(current.y - exit.y)
+
+dict[tuple[int, int], int]
