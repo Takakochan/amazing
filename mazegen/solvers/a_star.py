5e0069ab1a57cf3db8f82f0f155fe3312cff07ce
Takako Kunugi <tkunugi@f1r6s9.codam.nl>
2026-03-27 20:14:13 +0100

    Don't know what I should do with neighbors :D

--- mazegen/solvers/a_star.py
+++ mazegen/solvers/a_star.py
@@ -10,27 +10,44 @@ class SolverAStar(Solver):
         entry: Cell,
         exit: Cell,  # noqa: A002
     ) -> None:
-        pass
+        open_set = PriorityQue()
+        """ hopeful condidates list which we gonna check """
+        open_set.push(0, entry)
+        g_score = {(entry.x, entry.y): 0}
+        """g_score: actual cost sntry to current cell"""
+
+        closed_set = set()
+        while not open_set.is_empty():
+            """Main loop"""
+            current = open_set.pop() # 一番有望なのをPOP
+            if (current.x, current.y) in closed_set: # 入っていたらもう見てる
+                continue
+            closed_set.add((current.x, current.y))
+        #TODO 隣セルの処理
+        Grid.get_reachable_unmarked_neighbors(current)???
 
 class PriorityQue():
     def __init__(self):
-        self._heap = []
+        self._heap: List[Tuple[int, Cell]] = []
     
-    def push(self, priority: int, cell: Cell):
-
+    def push(self, priority: int, cell: Cell) -> None:
     
-    # def pop(self) -> Cell:
-    #     return
+    def pop(self) -> Cell:
+        return
 
-    # def is_empty(self)-> bool:
-    #     return 
+    def is_empty(self)-> bool:
+        if self._heap == 0:
+            return True
+        return False
 
 
 
-def f_scor(g_score: int, manhattan_heuristic: int) -> int:
+def f_score(g_score: int, manhattan_heuristic: int) -> int:
     return g_score + manhattan_heuristic
         
 def manhattan_heuristic(current: Cell, exit: Cell) -> int:
     return abs(current.x - exit.x) + abs(current.y - exit.y)
 
 dict[tuple[int, int], int]
+
+
